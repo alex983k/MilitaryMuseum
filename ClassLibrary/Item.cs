4b60fbecf5a4c6ae4d56f76d87af9eceea966334
alex983k <alex983k@edu.eal.dk>
2017-05-04 17:26:57 +0200

    Added a new property

--- ClassLibrary/Item.cs
+++ ClassLibrary/Item.cs
@@ -13,14 +13,16 @@ namespace ClassLibrary
         public string Area { get; private set; }
         public string Category { get; private set; }
         public string Description { get; private set; }
+        public string IDLocation { get; private set; }
         public int ProviderID { get; private set; }
-        public Item(int id, string name, string area, string category, string description, int providerID)
+        public Item(int id, string name, string area, string category, string description, string idlocation, int providerID)
         {
             ID = id;
             Name = name;
             Area = area;
             Category = category;
             Description = description;
+            IDLocation = idlocation;
             ProviderID = providerID;
         }
     }
